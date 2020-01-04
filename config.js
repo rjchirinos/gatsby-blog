97cfaac38b4367f40be9abc368e223b8667d3a7c
Raúl Chirinos <raulstuf@gmail.com>
2020-01-04 12:49:42 -0400

    First Commit

---
+++ config.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Raúl Chirinos",
+  subtitle: "Freelance Web Developer",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "UA-73379983-2",
+  useKatex: false,
+  menu: [
+    {
+      label: "Articles",
+      path: "/"
+    },
+    {
+      label: "About me",
+      path: "/pages/about"
+    },
+    {
+      label: "Contact me",
+      path: "/pages/contacts"
+    }
+  ],
+  author: {
+    name: "Raúl Chirinos",
+    photo: "/photo.jpg",
+    bio:
+      "Web Developer based in Venezuela. Love to work with individuals and startups to make the web a better place",
+    contacts: {
+      email: "raulstuf@gmail.com",
+      facebook: "",
+      telegram: "zenraul",
+      twitter: "raulstuf",
+      github: "rjchirinos",
+      rss: "",
+      vkontakte: "",
+      linkedin: "rjchirinos",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: ""
+    }
+  }
+};
